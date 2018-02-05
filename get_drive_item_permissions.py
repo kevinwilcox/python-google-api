1cd0ebc8de4785a58fbeff565709d3ad8cbded19
Kevin Wilcox <kevinwilcox>
2018-02-05 04:22:40 +0000

    minor comment edits

--- get_drive_item_permissions.py
+++ get_drive_item_permissions.py
@@ -1,14 +1,9 @@
 ###
 # In incident response it is critical to identify the scope of a compromise
-# This script attempts to give context to responders by retrieving all Team Drive
+# This script attempts to give context to responders by retrieving all Drive
 #   permissions based on any of the following criteria:
 #   o all Team Drives to which someone has access
-#   o all Team Drives whose name includes a given string
-#   o all Team Drives for the domain (last resort, this could be a mountain of data)
-#
-# The first iteration is Team Drives only
-# The second iteration will be to return all Drive items that are
-#   owned by or shared with a given user
+#   o all Drive items to which someone has access
 #
 # This script needs ONE of the following scopes:
 # https://www.googleapis.com/auth/drive
@@ -34,16 +29,9 @@ try:
   max_results   = args.count
 
 except Exception as e:
-  print("Error: couldn't assign a value to user_id")
+  print("Error: couldn't assign argument values")
   print(repr(e))
   print("This is a fatal error, exiting")
-  exit()
-
-if email_address == '':
-  print()
-  print("A user is required for this script. Please run again with --user <email_address>")
-  print("This is a fatal error, exiting...")
-  print()
   exit()
 
 try:
